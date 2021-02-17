649df78ff4c5202a186e50a69b98dd67285501d6
Kiera Carman <kiera.carman@gmail.com>
2021-02-17 09:43:47 -0500

    Add fields to user model

--- models/Users.js
+++ models/Users.js
@@ -11,6 +11,18 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('users', UserSchema);
