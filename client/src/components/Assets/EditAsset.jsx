f67d40d8d18322c7e0f7dc68702a5f54089f344d
Kiera Carman <kiera.carman@gmail.com>
2022-03-22 14:40:41 -0400

    Set placeholders for select elements

--- client/src/components/Assets/EditAsset.jsx
+++ client/src/components/Assets/EditAsset.jsx
@@ -27,18 +27,22 @@ const EditAsset = (props) => {
               <div className='form-field'><label>Acquisition Date</label><input onChange={handleChange} value={updateAsset.acquisitionDate} id='acquisitionDate' /></div>
               <div className='form-field'><label>Acquisition Amount</label><input onChange={handleChange} value={updateAsset.transactionAmount} id='transactionAmount' /></div>
               <div className='form-field'><label>Status</label><select onChange={handleChange} value={updateAsset.status} id='status'>
+                <option value='' disabled selected hidden>Select...</option>
                 <option value='in'>in</option>
                 <option value='out'>out</option>
               </select></div>
               <div className='form-field'><label>Asset Type</label><select onChange={handleChange} value={updateAsset.assetType} id='assetType'>
+                <option value='' disabled selected hidden>Select...</option>
                 <option value='hardware'>hardware</option>
                 <option value='software'>software</option>
               </select></div>
               <div className='form-field'><label>Maintenance Status</label><select onChange={handleChange} value={updateAsset.maintenanceStatus} id='maintenanceStatus'>
+                <option value='' disabled selected hidden>Select...</option>
                 <option value='active'>active</option>
                 <option value='inactive'>inactive</option>
               </select></div>
               <div className='form-field'><label>Minimum Access Level</label><select onChange={handleChange} value={updateAsset.minimumAccessLevel} id='minimumAccessLevel'>
+                <option value='' disabled selected hidden>Select...</option>
                 <option value='sophomore'>sophomore</option>
                 <option value='junior'>junior</option>
                 <option value='senior'>senior</option>
