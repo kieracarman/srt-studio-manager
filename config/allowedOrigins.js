59ed74284eeae8f6cacd645d92a6eb7bfe9531b4
Kiera Carman <kiera.carman@gmail.com>
2022-09-01 14:12:16 -0400

    Update allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ['http://localhost:3000']
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://srt-studio-manager.herokuapp.com'
+]
 
 export default allowedOrigins
