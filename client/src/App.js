f5dbe602ad859a492a6a9c929784e02f43099dc6
Kiera Carman <kiera.carman@gmail.com>
2022-09-07 13:28:54 -0400

    Add require auth component

--- client/src/App.js
+++ client/src/App.js
@@ -2,107 +2,136 @@ import { Routes, Route } from 'react-router-dom'
 
 import './App.css'
 import { Layout, Modal } from './components'
-import { Login, Prefetch, PersistLogin } from './features/auth'
+import { Login, Prefetch, PersistLogin, RequireAuth } from './features/auth'
 import { Dashboard, Assets, Bookings, Tickets, Users } from './features'
 import { EditAsset, NewAsset } from './features/assets/components'
 import { EditUser, NewUser } from './features/users/components'
 import { EditTicket, NewTicket } from './features/tickets/components'
 import { EditBooking, NewBooking } from './features/bookings/components'
+import Student from './features/student/Student'
+import { ROLES } from './config/roles'
+import useAuth from './hooks/useAuth'
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path='login' element={<Login />} />
+  const { role } = useAuth()
 
-      <Route element={<PersistLogin />}>
-        <Route element={<Prefetch />}>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<Dashboard />} />
+  let content
 
-            <Route path='assets'>
-              <Route index element={<Assets />} />
-              <Route
-                path=':id'
-                element={
-                  <Modal onClose='/assets'>
-                    <EditAsset />
-                  </Modal>
-                }
-              />
-              <Route
-                path='new'
-                element={
-                  <Modal onClose='/assets'>
-                    <NewAsset />
-                  </Modal>
-                }
-              />
-            </Route>
+  if (role === ROLES.admin || role === ROLES.supervisor) {
+    content = (
+      <Routes>
+        <Route path='login' element={<Login />} />
 
-            <Route path='users'>
-              <Route index element={<Users />} />
-              <Route
-                path=':id'
-                element={
-                  <Modal onClose='/users'>
-                    <EditUser />
-                  </Modal>
-                }
-              />
-              <Route
-                path='new'
-                element={
-                  <Modal onClose='/users'>
-                    <NewUser />
-                  </Modal>
-                }
-              />
-            </Route>
+        <Route element={<PersistLogin />}>
+          <Route
+            element={
+              <RequireAuth allowedRoles={[ROLES.admin, ROLES.supervisor]} />
+            }
+          >
+            <Route element={<Prefetch />}>
+              <Route path='/' element={<Layout />}>
+                <Route index element={<Dashboard />} />
 
-            <Route path='tickets'>
-              <Route index element={<Tickets />} />
-              <Route
-                path=':id'
-                element={
-                  <Modal onClose='/tickets'>
-                    <EditTicket />
-                  </Modal>
-                }
-              />
-              <Route
-                path='new'
-                element={
-                  <Modal onClose='/tickets'>
-                    <NewTicket />
-                  </Modal>
-                }
-              />
-            </Route>
+                <Route path='assets'>
+                  <Route index element={<Assets />} />
+                  <Route
+                    path=':id'
+                    element={
+                      <Modal onClose='/assets'>
+                        <EditAsset />
+                      </Modal>
+                    }
+                  />
+                  <Route
+                    path='new'
+                    element={
+                      <Modal onClose='/assets'>
+                        <NewAsset />
+                      </Modal>
+                    }
+                  />
+                </Route>
 
-            <Route path='bookings'>
-              <Route index element={<Bookings />} />
-              <Route
-                path=':id'
-                element={
-                  <Modal onClose='/bookings'>
-                    <EditBooking />
-                  </Modal>
-                }
-              />
-              <Route
-                path='new'
-                element={
-                  <Modal onClose='/bookings'>
-                    <NewBooking />
-                  </Modal>
-                }
-              />
+                <Route path='users'>
+                  <Route index element={<Users />} />
+                  <Route
+                    path=':id'
+                    element={
+                      <Modal onClose='/users'>
+                        <EditUser />
+                      </Modal>
+                    }
+                  />
+                  <Route
+                    path='new'
+                    element={
+                      <Modal onClose='/users'>
+                        <NewUser />
+                      </Modal>
+                    }
+                  />
+                </Route>
+
+                <Route path='tickets'>
+                  <Route index element={<Tickets />} />
+                  <Route
+                    path=':id'
+                    element={
+                      <Modal onClose='/tickets'>
+                        <EditTicket />
+                      </Modal>
+                    }
+                  />
+                  <Route
+                    path='new'
+                    element={
+                      <Modal onClose='/tickets'>
+                        <NewTicket />
+                      </Modal>
+                    }
+                  />
+                </Route>
+
+                <Route path='bookings'>
+                  <Route index element={<Bookings />} />
+                  <Route
+                    path=':id'
+                    element={
+                      <Modal onClose='/bookings'>
+                        <EditBooking />
+                      </Modal>
+                    }
+                  />
+                  <Route
+                    path='new'
+                    element={
+                      <Modal onClose='/bookings'>
+                        <NewBooking />
+                      </Modal>
+                    }
+                  />
+                </Route>
+              </Route>
             </Route>
           </Route>
         </Route>
-      </Route>
-    </Routes>
-  )
+      </Routes>
+    )
+  } else {
+    content = (
+      <Routes>
+        <Route path='login' element={<Login />} />
+
+        <Route element={<PersistLogin />}>
+          <Route element={<RequireAuth allowedRoles={[ROLES.basic]} />}>
+            <Route path='/' element={<Student />} />
+          </Route>
+        </Route>
+      </Routes>
+    )
+  }
+
+  return content
 }
 
 export default App
--- client/src/features/auth/index.js
+++ client/src/features/auth/index.js
@@ -1,3 +1,4 @@
 export { default as Login } from './Login'
 export { default as Prefetch } from './Prefetch'
 export { default as PersistLogin } from './PersistLogin'
+export { default as RequireAuth } from './RequireAuth'
