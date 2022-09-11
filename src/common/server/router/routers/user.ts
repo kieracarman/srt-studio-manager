3cdb2f6f76eb4edde1332fd45c79cfdd5cc7c63d
Kiera Carman <kiera.carman@gmail.com>
2022-09-10 22:10:50 -0400

    Add user routes

---
+++ src/common/server/router/routers/user.ts
@@ -0,0 +1,83 @@
+import { createRouter } from '../context'
+import { z } from 'zod'
+
+const userSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string().email(),
+  role: z.string(),
+  accessLevel: z.string()
+})
+
+export const userRouter = createRouter()
+  .query('getAll', {
+    async resolve({ ctx }) {
+      return await ctx.prisma.user.findMany()
+    }
+  })
+  .query('getOne', {
+    input: z.object({
+      id: z.string().cuid()
+    }),
+    async resolve({ ctx, input }) {
+      const { id } = input
+
+      const user = await ctx.prisma.user.findUnique({
+        where: { id },
+        include: {
+          accounts: true,
+          sessions: true,
+          bookings: true,
+          tickets: true
+        }
+      })
+
+      if (!user) {
+        throw new Error(`No user with id: ${id}`)
+      }
+
+      return user
+    }
+  })
+  .mutation('add', {
+    input: z.object({
+      data: userSchema
+    }),
+    async resolve({ ctx, input }) {
+      const { data } = input
+
+      const user = await ctx.prisma.user.create({
+        data
+      })
+
+      return user
+    }
+  })
+  .mutation('edit', {
+    input: z.object({
+      id: z.string().cuid(),
+      data: userSchema
+    }),
+    async resolve({ ctx, input }) {
+      const { id, data } = input
+
+      const user = await ctx.prisma.user.update({
+        where: { id },
+        data
+      })
+
+      return user
+    }
+  })
+  .mutation('delete', {
+    input: z.object({
+      id: z.string()
+    }),
+    async resolve({ ctx, input }) {
+      const { id } = input
+
+      await ctx.prisma.user.delete({ where: { id } })
+
+      return { id }
+    }
+  })
