9123a044ef2b083c9779f92b7dcc9ade307b68c0
Kiera Carman <kiera.carman@gmail.com>
2022-10-20 17:04:12 -0400

    Refactor using tailwind styles

--- src/common/components/ui/Navbar.tsx
+++ src/common/components/ui/Navbar.tsx
@@ -7,7 +7,6 @@ import {
   LogOut
 } from 'react-feather'
 
-import styles from './Navbar.module.css'
 import { NavLink } from '@components/ui'
 
 const items = [
@@ -21,7 +20,7 @@ const items = [
 
 const Navbar = () => {
   const content = (
-    <section className={styles.navbar}>
+    <section className='m-0 grid grid-cols-6 place-items-center border-t border-t-gray-200 p-0 text-center md:h-full md:grid-cols-1 md:grid-rows-[auto_auto_auto_auto_auto_1fr_auto] md:border-r md:border-t-0 md:border-r-gray-200 xl:w-60'>
       {items.map((item) => (
         <NavLink key={item.name} href={`/${item.path}`}>
           {item.icon}
