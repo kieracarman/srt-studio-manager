361f9c3dd707bfca2307a0abad0db206263a0760
Kiera Carman <kiera.carman@gmail.com>
2022-11-03 18:52:21 -0400

    Update layout

--- src/common/components/ui/Layout.tsx
+++ src/common/components/ui/Layout.tsx
@@ -1,15 +1,14 @@
 import { PropsWithChildren } from 'react'
 
-import styles from './Layout.module.css'
 import { Topbar } from '@components/ui'
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className={styles.container}>
-      <header className={styles.topbar}>
+    <div>
+      <header>
         <Topbar />
       </header>
-      <main className={styles.content}>{children}</main>
+      <main>{children}</main>
     </div>
   )
 }
