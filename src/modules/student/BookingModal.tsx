9673c18626f0f6ce266d1781c36350e90809bee1
Kiera Carman <kiera.carman@gmail.com>
2022-11-16 12:19:51 -0500

    Fix modal styles

--- src/modules/student/BookingModal.tsx
+++ src/modules/student/BookingModal.tsx
@@ -44,10 +44,10 @@ const BookingModal = ({ open, setOpen }: BookingModalProps) => {
               leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
             >
               <Dialog.Panel className='relative transform overflow-visible rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
-                <div className='px-4 pt-5 sm:p-6 sm:pb-4'>
+                <div className='px-4 pt-5 sm:p-6 sm:pb-0'>
                   <StudentBookingForm onCompleted={() => setOpen(false)} />
                 </div>
-                <div className='flex flex-col gap-3 px-4 py-3 sm:flex-row-reverse sm:px-6'>
+                <div className='flex flex-col gap-3 px-4 py-3 sm:px-6'>
                   <Button
                     variant='secondary'
                     onClick={() => setOpen(false)}
